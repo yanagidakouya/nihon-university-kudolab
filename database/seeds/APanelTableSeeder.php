ec7e3ba5555ec738351ac9787181203eba371d1e
kuy yngd <yanacod@gmail.com>
2021-07-08 12:05:38 +0900

    seeder変更

--- database/seeds/APanelTableSeeder.php
+++ database/seeds/APanelTableSeeder.php
@@ -15,17 +15,29 @@ class APanelTableSeeder extends Seeder
         //
 
 
-        for($i=1; $i<50; $i++) {
-            $vol = mt_rand(10, 50);
-            $cur = mt_rand(0, 10);
-            $pow = $vol * $cur;
-            DB::table('a_panels')->insert([
-                'voltage' => $vol,
-                'current' => $cur,
-                'power' => $pow,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
+        for($month=1; $month<4; $month++) {
+            for($day=1; $day<32; $day++) {
+                for($hour=0; $hour<24; $hour++) {
+                    for($m=0; $m<2; $m++) {
+                        $vol = mt_rand(10, 50);
+                        $cur = mt_rand(0, 10);
+                        $pow = $vol * $cur;
+                        if($m == 0) { $minute = '30';}
+                        if($m == 1) { $minute = '00'; }
+                        DB::table('a_panels')->insert([
+                            'voltage' => $vol,
+                            'current' => $cur,
+                            'power' => $pow,
+                            'created_at' => Carbon::parse(
+                                '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
+                            )->format('Y-m-d H:i:s'),
+                            'updated_at' => Carbon::parse(
+                                '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
+                            )->format('Y-m-d H:i:s'),
+                        ]);
+                    }
+                }
+            }
         }
 
     }
