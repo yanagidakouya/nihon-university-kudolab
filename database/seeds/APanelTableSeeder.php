1b1bb927881e7d12a2e44ac833cceac34cb7185a
kuy yngd <yanacod@gmail.com>
2021-11-20 12:32:14 +0900

    seederを本番に近いデータに変更

--- database/seeds/APanelTableSeeder.php
+++ database/seeds/APanelTableSeeder.php
@@ -13,32 +13,63 @@ class APanelTableSeeder extends Seeder
     public function run()
     {
         //
-
-
-        for($month=4; $month<8; $month++) {
-            for($day=1; $day<32; $day++) {
-                for($hour=0; $hour<24; $hour++) {
-                    for($m=0; $m<2; $m++) {
-                        $vol = mt_rand(10, 50);
-                        $cur = mt_rand(0, 10);
-                        $pow = $vol * $cur;
-                        if($m == 0) { $minute = '30';}
-                        if($m == 1) { $minute = '00'; }
-                        DB::table('a_panels')->insert([
-                            'voltage' => $vol,
-                            'current' => $cur,
-                            'power' => $pow,
-                            'created_at' => Carbon::parse(
-                                '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
-                            )->format('Y-m-d H:i:s'),
-                            'updated_at' => Carbon::parse(
-                                '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
-                            )->format('Y-m-d H:i:s'),
-                        ]);
-                    }
-                }
+        for($i=0; $i<10000; $i++) {
+            $vol = mt_rand(10, 50);
+            $cur = mt_rand(0, 10);
+            $pow = $vol * $cur;
+            $month = mt_rand(10, 12);
+            $day = mt_rand(1, 30);
+            $minute = mt_rand(0, 59);
+            $hour = mt_rand(0, 23);
+            if($month < 10) {
+                sprintf('%02d', $month);
             }
+            if($minute < 10) {
+                sprintf('%02d', $minute);
+            }
+            if($hour < 10) {
+                sprintf('%02d', $hour);
+            }
+            if($day < 10) {
+                sprintf('%02d', $day);
+            }
+            DB::table('a_panels')->insert([
+                'voltage' => $vol,
+                'current' => $cur,
+                'power' => $pow,
+                'created_at' => Carbon::parse(
+                    '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
+                )->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::parse(
+                    '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
+                )->format('Y-m-d H:i:s'),
+            ]);
         }
+
+        // for($month=4; $month<8; $month++) {
+        //     for($day=1; $day<32; $day++) {
+        //         for($hour=0; $hour<24; $hour++) {
+        //             for($m=0; $m<2; $m++) {
+        //                 $vol = mt_rand(10, 50);
+        //                 $cur = mt_rand(0, 10);
+        //                 $pow = $vol * $cur;
+        //                 if($m == 0) { $minute = '30';}
+        //                 if($m == 1) { $minute = '00'; }
+        //                 DB::table('a_panels')->insert([
+        //                     'voltage' => $vol,
+        //                     'current' => $cur,
+        //                     'power' => $pow,
+        //                     'created_at' => Carbon::parse(
+        //                         '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
+        //                     )->format('Y-m-d H:i:s'),
+        //                     'updated_at' => Carbon::parse(
+        //                         '2021-'. $month . '-' . $day . ' ' .$hour . ':' . $minute . ':00'
+        //                     )->format('Y-m-d H:i:s'),
+        //                 ]);
+        //             }
+        //         }
+        //     }
+        // }
 
     }
 }
